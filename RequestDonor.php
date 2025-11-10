53d3afc9b5c8d6f38046f7f4504cab537fc78dee
Dhevagar <p22014630@student.newinti.edu.my>
2025-11-10 09:03:16 +0800

    added request donor

---
+++ RequestDonor.php
@@ -0,0 +1,24 @@
+<?php
+$conn = new mysqli('localhost', 'root', '', 'blood_donation')
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+    $req_id = $_POST['requester'];
+    $bloodtype = $_POST['blood'];
+    $hospital = $_POST['hospital_name'];
+    $urgency = $_POST['urgency'];
+    $stat = $_POST['state'];
+
+    $sql = "INSERT INTO request (requester_id, blood_type, healthcare_name, urgency_level, status)
+            VALUES ('$req_id', '$bloodtype', '$hospital', '$urgency', '$stat')";
+    
+    if ($conn->query($sql)) {
+        echo "<script>alert('Request sent!')</script>";
+    } else {
+        echo "Error inserting data!";
+    }
+}
+$conn->close();
+?>
