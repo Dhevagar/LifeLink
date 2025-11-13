1f45ea6aad6edee1b0347b37fb872cb5cc145b98
Dhevagar <p22014630@student.newinti.edu.my>
2025-11-13 22:04:23 +0800

    Update HTML tag to include language attribute

--- LifeLink/admin.php
+++ LifeLink/admin.php
@@ -8,7 +8,7 @@ if (!isset($_SESSION['admin_logged_in'])) {
 }
 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
   <title>Admin Dashboard - LifeLink</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
