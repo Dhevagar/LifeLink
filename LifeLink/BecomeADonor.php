702f8a686bbb55f70c93e880b9ffc820635fd912
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-13 08:41:40 +0800

    Add Navigation bar

--- LifeLink/BecomeADonor.php
+++ LifeLink/BecomeADonor.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  <meta charset="UTF-8">
+  <title>LifeLink - Become a Donor</title>
+  <link rel="stylesheet" href="style.css">
 <body>
+
+ <nav class="navbar">
+    <div class="nav-container">
+        <a href="index.html" class="logo">
+        <img src="LifeLinkLogo.png" alt="LifeLink Logo">
+        </a>
+        <ul class="nav-links">
+        <li><a href="index.html">Homepage</a></li>
+        <li><a href="BecomeADonor.php" class="active">Become a Donor</a></li>
+        <li><a href="FindADonor.php">Find a Donor</a></li>
+        </ul>
+    </div>
+ </nav>
 
 </head>
 </body>
