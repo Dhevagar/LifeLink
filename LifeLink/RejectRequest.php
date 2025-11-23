7dff5e54424c40041741fe767781282160deacfe
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-23 22:08:57 +0800

    Create RejectRequest.php

---
+++ LifeLink/RejectRequest.php
@@ -0,0 +1,13 @@
+<?php
+include('connection.php');
+
+if (isset($_GET['request_id'])) {
+    $request_id = $_GET['request_id'];
+    $stmt = $conn->prepare("UPDATE requests SET status='Rejected' WHERE request_id=?");
+    $stmt->bind_param("i", $request_id);
+    $stmt->execute();
+}
+
+header("Location: admin.php");
+exit();
+?>
