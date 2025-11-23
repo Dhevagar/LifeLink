554a8fb606b92af8c7006400cee5c5a7c6870127
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-23 22:07:21 +0800

    Create ApproveDonor.php

---
+++ LifeLink/ApproveDonor.php
@@ -0,0 +1,14 @@
+<?php
+include('connection.php');
+
+
+if (isset($_GET['donor_id'])) {
+    $donor_id = $_GET['donor_id'];
+    $stmt = $conn->prepare("UPDATE donor SET status='Approved' WHERE donor_id=?");
+    $stmt->bind_param("i", $donor_id);
+    $stmt->execute();
+}
+
+header("Location: admin.php");
+exit();
+?>
