4ef9d0cf043afe41a44c616446cf70103f45a57b
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-23 22:37:02 +0800

    Update RejectRequest.php

--- LifeLink/RejectRequest.php
+++ LifeLink/RejectRequest.php
@@ -3,7 +3,7 @@ include('connection.php');
 
 if (isset($_GET['request_id'])) {
     $request_id = $_GET['request_id'];
-    $stmt = $conn->prepare("UPDATE requests SET status='Rejected' WHERE request_id=?");
+    $stmt = $conn->prepare("UPDATE requests SET request_status='Rejected' WHERE request_id=?");
     $stmt->bind_param("i", $request_id);
     $stmt->execute();
 }
