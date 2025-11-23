6ce2c7a13c6b51f77f8576cb9886d970205ee350
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-23 22:08:45 +0800

    Create RejectDonor.php

---
+++ LifeLink/RejectDonor.php
@@ -0,0 +1,13 @@
+<?php
+include('connection.php');
+
+if (isset($_GET['donor_id'])) {
+    $donor_id = $_GET['donor_id'];
+    $stmt = $conn->prepare("UPDATE donor SET status='Rejected' WHERE donor_id=?");
+    $stmt->bind_param("i", $donor_id);
+    $stmt->execute();
+}
+
+header("Location: admin.php");
+exit();
+?>
