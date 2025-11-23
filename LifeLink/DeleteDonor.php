29d75222647d92e6bb7bf3787213dd135092d689
HanYi2002 <p23015098@student.newinti.edu.my>
2025-11-23 21:01:47 +0800

    Update DeleteDonor.php

--- LifeLink/DeleteDonor.php
+++ LifeLink/DeleteDonor.php
@@ -1,7 +1,6 @@
 <?php
 include 'connection.php';
 
-// Check if donor_id is provided in the URL
 if (!isset($_GET['donor_id'])) {
     echo "<script>alert('Invalid donor ID'); window.location.href='admin.php';</script>";
     exit;
@@ -9,7 +8,6 @@ if (!isset($_GET['donor_id'])) {
 
 $donor_id = $_GET['donor_id'];
 
-// Prepare delete statement
 $sql = "DELETE FROM donor WHERE donor_id = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $donor_id);
