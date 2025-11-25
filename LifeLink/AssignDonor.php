ed318bb5c594c7282926d356c941811cdb0efdc8
Dhevagar <p22014630@student.newinti.edu.my>
2025-11-25 15:52:00 +0800

    Update AssignDonor.php

--- LifeLink/AssignDonor.php
+++ LifeLink/AssignDonor.php
@@ -5,9 +5,13 @@ if (!isset($_GET['request_id'])) {
     header("Location: admin.php");
     exit();
 }
+$donor_stmt = $conn->prepare("SELECT donor_id, full_name, blood_type FROM donor ORDER BY full_name ASC");
+$donor_stmt->execute();
 
 $request_id = intval($_GET['request_id']);
+$donor_list = $donor_stmt->get_result();
 
 ?>
+
 
 
