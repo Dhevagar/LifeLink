0b3d39b2dbf647c18b432a8d07e3a19397e6aedf
Dhevagar <p22014630@student.newinti.edu.my>
2025-11-25 15:49:10 +0800

    Update AssignDonor.php

--- LifeLink/AssignDonor.php
+++ LifeLink/AssignDonor.php
@@ -1,14 +1,6 @@
 <?php
 include('connection.php');
 
-if (isset($_GET['request_id']) && isset($_GET['donor_id'])) {
-    $request_id = $_GET['request_id'];
-    $donor_id = $_GET['donor_id'];
-    $stmt = $conn->prepare("UPDATE requests SET assigned_donor_id=? WHERE request_id=?");
-    $stmt->bind_param("ii", $donor_id, $request_id);
-    $stmt->execute();
-}
 
-header("Location: admin.php");
-exit();
 ?>
+
