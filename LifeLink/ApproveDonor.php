1d1c3446a95a4b9a379863b2894b9c458185d88d
Dhevagar <p22014630@student.newinti.edu.my>
2025-11-26 21:43:43 +0800

    Update ApproveDonor.php

--- LifeLink/ApproveDonor.php
+++ LifeLink/ApproveDonor.php
@@ -1,14 +1,1 @@
-<?php
-include('connection.php');
 
-
-if (isset($_GET['donor_id'])) {
-    $donor_id = $_GET['donor_id'];
-    $stmt = $conn->prepare("UPDATE donor SET donor_status='Approved' WHERE donor_id=?");
-    $stmt->bind_param("i", $donor_id);
-    $stmt->execute();
-}
-
-header("Location: admin.php");
-exit();
-?>
